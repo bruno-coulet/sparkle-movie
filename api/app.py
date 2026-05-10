48e2e33893e4beb70cdaffca006196e966c2c993
Bruno Coulet <bruno.coulet@laplateforme.io>
2026-05-10 23:51:38 +0200

    Final TLS and path fixes

--- api/app.py
+++ api/app.py
@@ -26,7 +26,8 @@ app = FastAPI(
     title="Sparkle Movie API",
     description="API de recommandation de films basée sur Spark et le dataset MovieLens.",
     root_path="/sparkle-movie/api",
-    openapi_url="/openapi.json" # Force le chemin relatif pour OpenAPI (Swagger)
+    openapi_url="/openapi.json", # Force le chemin relatif pour OpenAPI (Swagger)
+    docs_url="/docs",
     )
 
 # Session Spark partagée (lazy initialization)
